90b65b6470b3de66524f67e51a503c1fe6186b7e
DEVU R KURUP <devukurup199918@gmail.com>
2021-09-13 20:58:07 +0530

    added click handling functions for image buttons

--- app.js
+++ app.js
@@ -9,11 +9,17 @@ bling.style.cssText = "text-align:center; font-weight: bold ;text-shadow: 2px 1p
 root.appendChild(bling);
 
 
+
+const divRoot = document.createElement('div');
+divRoot.style.display = "flex";
+root.appendChild(divRoot);
+
 /* Create div element for the contents */
+ 
 
 const divMain = document.createElement('div');
 divMain.style.cssText = "display:flex; flex-direction: column ";
-root.appendChild(divMain);
+divRoot.appendChild(divMain);
 
 const pOne = document.createElement('p');
 pOne.textContent = "Sizzle your life";
@@ -30,17 +36,17 @@ divOne.style.cssText = "display: flex;";
 divMain.appendChild(divOne);
 
 
-/* adding set of spans  for div one*/
+/* adding set of buttons  for div one*/
 
-const polaroid = document.createElement('span');
+const polaroid = document.createElement('button');
 polaroid.textContent = "Polaroid";
-const tv = document.createElement('span');
+const tv = document.createElement('button');
 tv.textContent = "TV";
-const traitor = document.createElement('span');
+const traitor = document.createElement('button');
 traitor.textContent = "Traitor";
-const fallGuy = document.createElement('span');
+const fallGuy = document.createElement('button');
 fallGuy.textContent = "Fall Guy";
-const radio = document.createElement('span');
+const radio = document.createElement('button');
 radio.textContent = "Radio";
 
 divOne.appendChild(polaroid);
@@ -48,6 +54,7 @@ divOne.appendChild(tv);
 divOne.appendChild(traitor);
 divOne.appendChild(fallGuy);
 divOne.appendChild(radio);
+
 
 
 
@@ -67,7 +74,9 @@ divTwo.appendChild(inp);
 const selector = document.createElement("Select");
 selector.setAttribute("id","sel");
 divTwo.appendChild(selector);
-
+selector.addEventListener('click', (event)=>{
+    root.style.backgroundColor = "#FFFAFA";
+})
 
 /* some set of color names and hex values for reference */
 
@@ -113,8 +122,93 @@ let color = {
     selector.appendChild(opt);
 }
 
+ /* create radio buttons */
+
+
+ const radioDiv = document.createElement("div");
+ radioDiv.style.cssText = "display: flex";
+ divTwo.appendChild(radioDiv);
+
+
+ const blob = document.createElement('input');
+ blob.setAttribute("type","radio");
+ blob.setAttribute("name","rad");
+ blob.setAttribute("value","blob");
+ radioDiv.appendChild(blob);
+ 
+ const blobspan = document.createElement("span");
+ blobspan.textContent = "Blob";
+ radioDiv.appendChild(blobspan);
+
+ const nope = document.createElement('input');
+ nope.setAttribute("type","radio");
+ nope.setAttribute("name","rad");
+ nope.setAttribute("value","nope");
+ radioDiv.appendChild(nope);
+
+ const nopespan = document.createElement("span");
+ nopespan.textContent = "Nope";
+ radioDiv.appendChild(nopespan);
+
+
+ /* add camera button icon*/ 
+
+ const camera = document.createElement("button");
+ divMain.appendChild(camera);
+ camera.style.backgroundImage = "url('./assets/images/camera.png')";
+ camera.style.width = '20px';
+ camera.style.height = '20px';
+ camera.style.padding = '15px';
+ camera.style.backgroundColor = 'white';
+ camera.style.backgroundSize = 'contain';
+ camera.style.backgroundRepeat = 'no-repeat';
 
 
 
 
+ /* Create dynamic heading */
 
+const rightDiv = document.createElement('div');
+rightDiv.style.display = "flex";
+rightDiv.style.flexDirection = "column";
+divRoot.appendChild(rightDiv);
+
+
+
+ const dynHeading = document.createElement('h1');
+ rightDiv.appendChild(dynHeading);
+
+
+
+ /* function for dynamic typing */
+ inp.addEventListener("keyup",(event) => {
+    dynHeading.textContent = inp.value;
+  })
+
+
+
+  const imgDiv = document.createElement('div');
+  rightDiv.appendChild(imgDiv);
+
+
+
+  
+/* function for displaying images */
+polaroid.addEventListener("click", (event) => {
+    imgDiv.innerHTML = "<img src = './assets/images/polaroid.png'/>";
+})
+
+fallGuy.addEventListener("click", (event) => {
+    imgDiv.innerHTML = "<img src = './assets/images/fall-guy-01.png'/>";
+})
+radio.addEventListener("click", (event) => {
+    imgDiv.innerHTML = "<img src = './assets/images/radio-02.png'/>";
+})
+
+traitor.addEventListener("click", (event)=>{
+    imgDiv.innerHTML = "<img src = './assets/images/among-us.png'/>";
+})
+
+tv.addEventListener("click", (event)=>{
+    imgDiv.innerHTML = "<img src = './assets/images/tv.png'/>";
+})
